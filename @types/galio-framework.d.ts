5c722cb4b103cc5f4fccf3c31cd7c64cacf7581b
Shivam Malhotra <shivammalhotra08@gmail.com>
2020-10-25 17:04:32 +0530

    Add types

---
+++ @types/galio-framework.d.ts
@@ -0,0 +1,18 @@
+import Accordion from './Accordion';
+import Block from './Block';
+import Button from './Button';
+import Card from './Card';
+import Checkbox from './Checkbox';
+import DeckSwiper from './DeckSwiper';
+import Icon from './Icon';
+import Input from './Input';
+import NavBar from './NavBar';
+import Radio from './Radio';
+import Slider from './Slider';
+import Switch from './Switch';
+import Text from './Text';
+import Toast from './Toast';
+import theme, { withGalio, GalioProvider } from './theme';
+import galioConfig from './config/galio.json';
+declare const GalioFont: any;
+export { Accordion, Block, Button, Card, Checkbox, DeckSwiper, Icon, Input, NavBar, Radio, Slider, Text, Toast, Switch, theme, withGalio, GalioProvider, galioConfig, GalioFont, };
